f23d2a03386b6aae358437f6e8c69bf324c3c63b
Makra Flórián <makraflorian10@gmail.com>
2023-06-29 16:19:07 +0200

    initial commit

---
+++ ERCalculator/ERCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ERCalculatorApp.swift
+//  ERCalculator
+//
+//  Created by Makra Flórián Róbert on 2023. 06. 29..
+//
+
+import SwiftUI
+
+@main
+struct ERCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
