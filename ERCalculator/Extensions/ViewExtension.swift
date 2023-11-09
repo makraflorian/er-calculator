c84fae8ab6027b429ddbeda4945e8d3a78a4597d
Makra Flórián <makraflorian10@gmail.com>
2023-11-09 13:22:57 +0100

    haptic feedback modifier view extension

--- ERCalculator/Extensions/ViewExtension.swift
+++ ERCalculator/Extensions/ViewExtension.swift
@@ -13,4 +13,21 @@ extension View {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
                                         to: nil, from: nil, for: nil)
     }
+    
+    func hapticFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle = .medium) -> some View {
+        self.modifier(HapticFeedback(style: style))
+    }
+}
+
+struct HapticFeedback: ViewModifier {
+    private let generator: UIImpactFeedbackGenerator
+    
+    init(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        generator = UIImpactFeedbackGenerator(style: style)
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .onTapGesture(perform: generator.impactOccurred)
+    }
 }
